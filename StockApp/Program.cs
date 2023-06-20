77727b2d76281f78bc54e48f0d141946fd361d32
Benny Jiang <62633696+Bennyjiangzy@users.noreply.github.com>
2023-06-19 18:05:02 -0700

    Add project files.

---
+++ StockApp/Program.cs
@@ -0,0 +1,19 @@
+using Services;
+using ServiceContracts;
+using StockMarketSolution;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
+builder.Services.AddSingleton<IFinnhubService, FinnhubService>();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
